3b3b5e1f041b574670d14676b53ab5fe80bbe9a0
BepHuTe-CaMoKaT <55881526+BepHuTe-CaMoKaT@users.noreply.github.com>
2021-03-19 06:43:52 +0300

    error
    error

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,6 @@
 package main;
 
 import java.util.concurrent.*;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -12,8 +8,8 @@ public class Main {
         Thread t1 = new Thread();
         Thread t2 = new Thread();
         Thread t3 = new Thread();
-        CompletableFuture.runAsync(()->f.first(t1));
-        Thread.sleep(1000);
+
+
         CompletableFuture.runAsync(()-> {
             try {
                 f.second(t2);
@@ -30,8 +26,8 @@ public class Main {
                 e.printStackTrace();
             }
         });
-
-
+        CompletableFuture.runAsync(()->f.first(t1));
+        Thread.sleep(1000);
     }
 }
 
@@ -57,71 +53,71 @@ class Foo2{
         s2.release();
     }
 }
-class Foo{
-    public int threadNumber;
-
-    public Foo() {
-        this.threadNumber = 1;
-    }
-
-    synchronized void first(Runnable printFirst) throws InterruptedException {
-        while (threadNumber!=1) wait();
-        printFirst.run();
-        threadNumber++;
-        notifyAll();
-    }
-    synchronized void second(Runnable printSecond) throws InterruptedException {
-        while (threadNumber!=2) wait();
-        printSecond.run();
-        notifyAll();
-    }
-    synchronized void third(Runnable printThird) throws InterruptedException {
-        while (threadNumber!=3) wait();
-        printThird.run();
-        threadNumber++;
-        notifyAll();
-    }
-}
-class Foo1{
-    public int threadNumber;
-    private final Lock lock = new ReentrantLock();
-    private final Condition condition = lock.newCondition();
-
-    public Foo1() {
-        this.threadNumber = 1;
-    }
-
-    public void first(Runnable printFirst) throws InterruptedException{
-        lock.lock();
-        try {
-            printFirst.run();
-            this.threadNumber++;
-            condition.signalAll();
-        }finally {
-            lock.unlock();
-        }
-    }
-
-    public void second(Runnable printSecond) throws InterruptedException{
-        lock.lock();
-        try {
-            while (threadNumber!=2) condition.await();
-            printSecond.run();
-            this.threadNumber++;
-            condition.signalAll();
-        }finally {
-            lock.unlock();
-        }
-    }
-    public void third(Runnable printThird)throws InterruptedException{
-        lock.lock();
-        try {
-            while (threadNumber!=3) condition.await();
-            printThird.run();
-            this.threadNumber++;
-            condition.signalAll();
-        }finally {
-            lock.unlock();
-        }
-    }
-}
+//class Foo{
+//    public int threadNumber;
+//
+//    public Foo() {
+//        this.threadNumber = 1;
+//    }
+//
+//    synchronized void first(Runnable printFirst) throws InterruptedException {
+//        while (threadNumber!=1) wait();
+//        printFirst.run();
+//        threadNumber++;
+//        notifyAll();
+//    }
+//    synchronized void second(Runnable printSecond) throws InterruptedException {
+//        while (threadNumber!=2) wait();
+//        printSecond.run();
+//        notifyAll();
+//    }
+//    synchronized void third(Runnable printThird) throws InterruptedException {
+//        while (threadNumber!=3) wait();
+//        printThird.run();
+//        threadNumber++;
+//        notifyAll();
+//    }
+//}
+//class Foo1{
+//    public int threadNumber;
+//    private final Lock lock = new ReentrantLock();
+//    private final Condition condition = lock.newCondition();
+//
+//    public Foo1() {
+//        this.threadNumber = 1;
+//    }
+//
+//    public void first(Runnable printFirst) throws InterruptedException{
+//        lock.lock();
+//        try {
+//            printFirst.run();
+//            this.threadNumber++;
+//            condition.signalAll();
+//        }finally {
+//            lock.unlock();
+//        }
+//    }
+//
+//    public void second(Runnable printSecond) throws InterruptedException{
+//        lock.lock();
+//        try {
+//            while (threadNumber!=2) condition.await();
+//            printSecond.run();
+//            this.threadNumber++;
+//            condition.signalAll();
+//        }finally {
+//            lock.unlock();
+//        }
+//    }
+//    public void third(Runnable printThird)throws InterruptedException{
+//        lock.lock();
+//        try {
+//            while (threadNumber!=3) condition.await();
+//            printThird.run();
+//            this.threadNumber++;
+//            condition.signalAll();
+//        }finally {
+//            lock.unlock();
+//        }
+//    }
+//}
--- src/main/task2.java
+++ src/main/task2.java
@@ -1,55 +1,130 @@
 package main;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Semaphore;
+import java.util.LinkedList;
+import java.util.concurrent.*;
+import java.util.function.IntConsumer;
+
+
 
 public class task2 {
-    private static Semaphore s1 = new Semaphore(0);
-
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
+        FizzBuzzDemo.process(new FizzBuzz(15));
     }
 }
 
 class FizzBuzzDemo {
 
-    FizzBuzz fizzBuzz = new FizzBuzz(15);
+    public static String process(FizzBuzz fizzBuzz){
+        ExecutorService e = Executors.newFixedThreadPool(4);
+        final LinkedList<Future<?>> futures = new LinkedList<>();
+        final CopyOnWriteArrayList<String> out = new CopyOnWriteArrayList<>();
 
+        futures.add(e.submit(()->{
+            try {
+                fizzBuzz.fizz(()->out.add("fizz"));
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));
 
-    public void method() throws InterruptedException {
+        futures.add(e.submit(()->{
+            try {
+                fizzBuzz.buzz(()->out.add("buzz"));
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));
 
-        try {
-            Thread t1 = new Thread(fizzBuzz::fizz).start();
-            Thread t2 = new Thread(fizzBuzz::buzz).start();
-            Thread t3 = new Thread(fizzBuzz::fizzbuzz).start();
-            Thread t4 = new Thread(fizzBuzz::number).start();
-            Thread.sleep(2000);
-        } catch (
-                InterruptedException e) {
-            e.printStackTrace();
+        futures.add(e.submit(()->{
+            try {
+                fizzBuzz.fizzBuzz(()->out.add("fizzBuzz"));
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));
+
+        futures.add(e.submit(()->{
+            try {
+                fizzBuzz.number(x->out.add(""+x));
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }));
+
+        for (Future<?> f:futures){
+            try {
+                f.get(700, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException ex) {
+                ex.printStackTrace();
+            }
         }
+
+        return out.toString();
     }
-
-
 }
 
 class FizzBuzz {
-    private int number;
+    private int n;
+    private int count;
+
 
     public FizzBuzz(int n) {
-        this.number = n;
+        this.n = n;
+        this.count = n;
+    }
+    Semaphore fizz = new Semaphore(0);
+    Semaphore buzz = new Semaphore(0);
+    Semaphore fizzBuzz = new Semaphore(0);
+    Semaphore number = new Semaphore(1);
+    public void fizz(Runnable printFizz)throws InterruptedException {
+        while (count>0){
+            fizz.acquire();
+            if (count==0) break;
+            count--;
+            printFizz.run();
+            number.release();
+        }
+    }
+    public void buzz(Runnable printBuzz) throws InterruptedException{
+        while (count>0){
+            buzz.acquire();
+            if (count==0) break;
+            count--;
+            printBuzz.run();
+            number.release();
+        }
+    }
+    public void fizzBuzz(Runnable printFizzBuzz) throws InterruptedException{
+        while (count>0){
+            fizzBuzz.acquire();
+            if (count==0) break;
+            count--;
+            printFizzBuzz.run();
+            number.release();
+        }
     }
 
-    public void fizz(Runnable printFizz) throws InterruptedException {
+    public void number(IntConsumer printNumber) throws InterruptedException{
+        while (count!=0){
+            number.acquire();
+            if ((n-count+1)%5==0&&(n-count+1)%3==0){
+                fizzBuzz.release();
+                continue;
+            }
+            if ((n-count+1)%5==0){
+                buzz.release();
+                continue;
+            }
+            if ((n-count+1)%3==0){
+                fizz.release();
+                continue;
+            }
+            number.release();
+            printNumber.accept(n-count+1);
+            count--;
+        }
+        fizz.release();
+        buzz.release();
+        fizzBuzz.release();
     }
-
-    public void buzz(Runnable printBuzz) {
-    }
-
-    public void fizzbuzz(Runnable printFizzBuzz) {
-    }
-
-    public void number(Runnable printNumber) {
-    }
-
 }
